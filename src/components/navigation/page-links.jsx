8ebb64635265888c280a3ddc8608c7b72745c817
horhik <horhik@tuta.io>
2020-05-25 02:59:57 +0300

    create basic routing

--- src/components/navigation/page-links.jsx
+++ src/components/navigation/page-links.jsx
@@ -1,22 +1,36 @@
 import React from 'react'
 import asciiWord from "../../functions/ascii-word";
 import {Link} from "react-router-dom";
+import pages from "../../constants/routes";
+import {connect} from 'react-redux'
+import {setCurrentPage} from "../../actions/route-actions";
 
 const PageLinks = props => {
+    const goToPage = page => {
+        props.setCurrentPage(page)
+    }
     return(
         <ul className={'page-list'}>
-            <li className={'page-link'}>
-                <Link to={'/portfolio'}>
-                    {asciiWord('Portfolio')}
-                </Link>
-            </li>
-            <li className={'page-link'}>
-                    <Link to={'/blog'}>
-                    {asciiWord('Blog')}
-                    </Link>
-            </li>
+            {
+                pages.map(page => {
+                    if(page.src !== props.currentPageSrc){
+                        return(
+
+                            <li key={page.src} className={'page-link'}>
+                                <Link to={page.src} onClick={() => goToPage(page)}>
+                                    {asciiWord(page.name)}
+                                </Link>
+                            </li>
+                        )
+                    }
+                })
+            }
         </ul>
     )
 }
 
-export default PageLinks
+export default connect(state => ({
+    currentPageSrc: state.client.currentPath
+}), {
+    setCurrentPage
+})(PageLinks)
